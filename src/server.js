593aac22f933e20bacb61cbe07ede0bec3266f35
Ritika-Bhattia2177 <ritikakangra2005@gmail.com>
2026-05-07 10:37:58 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,14 +4,6 @@ dotenv.config();
 const app = require('./app');
 const connectDB = require('./config/db');
 
-const PORT = process.env.PORT || 5000;
+connectDB();
 
-const startServer = async () => {
-  await connectDB();
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-};
-
-startServer();
+module.exports = app;
